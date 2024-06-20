be8c31b35643568d0fa87d0ccfe048690032d372
exst3zz <annapavtso@gmail.com>
2024-06-20 11:44:19 +0000

    Deploying to gh-pages from @ exst3zz/goit-js-hw-11@d61c98a5d2f19df60c5f644a4cfacb9335efa80b 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,28 @@
+import{S as h,i as d}from"./assets/vendor-8c59ed88.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))r(t);new MutationObserver(t=>{for(const o of t)if(o.type==="childList")for(const i of o.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&r(i)}).observe(document,{childList:!0,subtree:!0});function a(t){const o={};return t.integrity&&(o.integrity=t.integrity),t.referrerPolicy&&(o.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?o.credentials="include":t.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function r(t){if(t.ep)return;t.ep=!0;const o=a(t);fetch(t.href,o)}})();function f(e){const a=`https://pixabay.com/api/?${new URLSearchParams({key:"44324365-be3070df1c049607fe38536c0",q:e,image_type:"photo",orientation:"horizontal",safesearch:!0})}`;return fetch(a).then(r=>{if(!r.ok)throw new Error(r.status);return r.json()}).catch(r=>console.log("Error fetching photos:",r))}function m(e){return e.map(p).join("")}function p(e){return`<li class="gallery-item">
+  <a href="${e.largeImageURL}" class="gallery-item-link"
+    ><img
+      class="gallery-item-img"
+      src="${e.webformatURL}"
+      alt="${e.tags}"
+      width="360"
+  /></a>
+  <ul class="photo-info-list">
+    <li class="photo-info-item">
+      <p class="photo-data-name">Likes</p>
+      <p class="photo-data">${e.likes}</p>
+    </li>
+    <li class="photo-info-item">
+      <p class="photo-data-name">Views</p>
+      <p class="photo-data">${e.views}</p>
+    </li>
+    <li class="photo-info-item">
+      <p class="photo-data-name">Comments</p>
+      <p class="photo-data">${e.comments}</p>
+    </li>
+    <li class="photo-info-item">
+      <p class="photo-data-name">Downloads</p>
+      <p class="photo-data">${e.downloads}</p>
+    </li>
+  </ul>
+</li>`}const g="/goit-js-hw-11/assets/close-784344e5.svg",u=document.querySelector(".search-form"),l=document.querySelector(".gallery"),n=document.querySelector(".loader"),y=new h(".gallery a",{captionsData:"alt",captionDelay:350});u.addEventListener("submit",L);function L(e){e.preventDefault();const s=e.target.elements.searchField.value.trim();if(s===""){l.innerHTML="",c("You forgot enter data for search","#ffa000");return}n.classList.remove("visually-hidden"),f(s).then(a=>{if(a.hits.length===0)l.innerHTML="",c("Sorry, there are no images matching your search query. Please try again!","#EF4040");else{const r=m(a.hits);l.innerHTML=r,y.refresh()}n.classList.add("visually-hidden")}).catch(a=>{c("An error occurred while fetching photos. Please try again later.","#EF4040"),n.classList.add("visually-hidden")}),u.reset()}function c(e,s){d.show({message:e,position:"topRight",backgroundColor:s,iconUrl:g,messageColor:"#fff",theme:"dark",maxWidth:"350px"})}
+//# sourceMappingURL=commonHelpers.js.map
